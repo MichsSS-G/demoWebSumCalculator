9651ef74f92fc4701feadf8e76493fcbd1ca2385
MichsSS-G <mich.gribakin.fml@yandex.ru>
2025-11-22 01:40:27 +0500

    Added html-form

--- src/main/java/com/example/helloWorld/HelloWorldApplication.java
+++ src/main/java/com/example/helloWorld/HelloWorldApplication.java
@@ -3,28 +3,35 @@ package com.example.helloWorld;
 import java.util.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
 
+	@GetMapping("/sum-form")
+	public String showSumForm() {
+		return "sum";
+	}
+
 	@GetMapping("/sum")
-	public String sum(@RequestParam(value = "numbers", required = false) List<Integer> numbers) {
+	public String sum(@RequestParam(value = "numbers", required = false) List<Integer> numbers, Model model) {
 		if (numbers == null || numbers.isEmpty()) {
-			return "No numbers provided. Use ?numbers=1&numbers=2&numbers=3";
+			model.addAttribute("result", "No numbers provided");
 		}
 		int result = 0;
 		for (int el : numbers) {
 			result += el;
 		}
-//		int total = numbers.stream().mapToInt(Integer::intValue).sum();
-		return "Sum: " + result;
+		model.addAttribute("result", result);
+		return "sum";
 	}
 }
